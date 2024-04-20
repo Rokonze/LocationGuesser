60ddc1b1e56928679038c8bc0d44f8d1fa48a2c1
Rokonze <143441424+Rokonze@users.noreply.github.com>
2024-04-21 01:03:26 +0300

    Add hamburger menu for small screens with custom hook for when clicking away

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Game from './components/Game'
 
 function App() {
   
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
-      
+      <Game />
     </>
   )
 }
