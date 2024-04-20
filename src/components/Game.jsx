ea83fffe6745be70fb630d680786c853b97cee21
Rokonze <143441424+Rokonze@users.noreply.github.com>
2024-04-21 01:17:10 +0300

    Create Play button

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Game() {
   return (
-    <div>Game</div>
+    <div className='h-[100vh] bg-[#F2F2F2] flex justify-center items-center '>
+        <button className='bg-[#F2811D] p-4 rounded-lg'>
+            <p className='text-white font-extrabold text-2xl'>PLAY</p>
+        </button>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,15 +19,35 @@ function Navbar() {
 
     
   return (
-    
-        <nav  className='bg-[#F2F2F2] text-[#262223] shadow-sm fixed top-0 left-0 right-0 '  >
+        <div className="relative">
+            <nav  className='bg-[#F2F2F2] text-[#262223] shadow-xl fixed top-0 left-0 right-0  '  >
             
-                <div className=' flex justify-between container mx-auto p-3'>
-                    <div className='flex text-3xl items-center'>
-                        <FaLocationDot className='text-[#F2811D] '/>
-                        <h1 className=' font-extrabold'>LocationGuesser</h1>
-                    </div>
-                    <ul className='text-xl space-x-8 hidden place-items-center  md:visible md:flex md:items-center'>
+            <div className=' flex justify-between container mx-auto p-3'>
+                <div className='flex text-3xl items-center'>
+                    <FaLocationDot className='text-[#F2811D] '/>
+                    <h1 className=' font-extrabold'>LocationGuesser</h1>
+                </div>
+                <ul className='text-xl space-x-8 hidden place-items-center  md:visible md:flex md:items-center'>
+                    <li className='p-2 hover:bg-[#dadada] rounded-xl'>
+                        <button>Home</button>
+                    </li>
+                    <li className='p-2 hover:bg-[#dadada] rounded-xl'>
+                        <button>About</button>
+                    </li>
+                    <li className='p-2 hover:bg-[#dadada] rounded-xl'>
+                        <button>Search</button>
+                    </li>
+                </ul>
+                <div className='flex items-center text-3xl md:hidden '>
+                    <button className='hover:bg-[#dadada] rounded-xl p-2' onClick={handleClick}>
+                        <AiOutlineMenu className='text-[#F2811D]  '/>
+                    </button>
+                </div>
+                
+            </div>
+            {isOpen && 
+                <div ref={ref} className="flex md:hidden flex-col items-center p-2">
+                    <ul className='text-xl space-x-2  place-items-center flex '>
                         <li className='p-2 hover:bg-[#dadada] rounded-xl'>
                             <button>Home</button>
                         </li>
@@ -38,32 +58,14 @@ function Navbar() {
                             <button>Search</button>
                         </li>
                     </ul>
-                    <div className='flex items-center text-3xl md:hidden '>
-                        <button className='hover:bg-[#dadada] rounded-xl p-2' onClick={handleClick}>
-                            <AiOutlineMenu className='text-[#F2811D]  '/>
-                        </button>
-                    </div>
-                    
                 </div>
-                {isOpen && 
-                    <div ref={ref} className="flex md:hidden flex-col items-center p-2">
-                        <ul className='text-xl space-x-2  place-items-center flex '>
-                            <li className='p-2 hover:bg-[#dadada] rounded-xl'>
-                                <button>Home</button>
-                            </li>
-                            <li className='p-2 hover:bg-[#dadada] rounded-xl'>
-                                <button>About</button>
-                            </li>
-                            <li className='p-2 hover:bg-[#dadada] rounded-xl'>
-                                <button>Search</button>
-                            </li>
-                        </ul>
-                    </div>
-                }
-            
-            
-            
-        </nav> 
+            }
+        
+        
+        
+    </nav> 
+        </div>
+        
         
         
     
